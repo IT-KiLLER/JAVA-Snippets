c0e42964a016e6191dcd7d39ca83b6cb3d92095b
IT-KiLLER <it-killer@protonmail.com>
2017-11-01 11:20:54 +0100

    Add files via upload

--- Files/HighestNumbers.java
+++ Files/HighestNumbers.java
@@ -12,15 +12,14 @@ public class HighestNumbers {
 		numbers.add(new Integer(1));
 		numbers.add(new Integer(9));
 	
-		
 		// [50, 2, 1, 9] to 95021
 		Collections.sort(numbers, new Comparator<Integer>() {
-		    @Override
-		    public int compare(Integer a, Integer b) {
-			    String ab = String.format("%d%d", a, b);
-			    String ba = String.format("%d%d", b, a);
-			    return ba.compareTo(ab);
-		     }
+			@Override
+			public int compare(Integer a, Integer b) {
+				String ab = String.format("%d%d", a, b);
+				String ba = String.format("%d%d", b, a);
+				return ba.compareTo(ab);
+			}
 		});
 
 		StringBuilder output = new StringBuilder();
